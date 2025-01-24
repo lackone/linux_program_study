9ba3233ae9f991dd026f24ef759740375486316f
lackone <lackone@126.com>
2025-01-24 13:06:46 +0800

    添加

--- 44_multicast_broadcast/main.cpp
+++ 44_multicast_broadcast/main.cpp
@@ -31,6 +31,8 @@ using namespace std;
 // join.imr_interface.s_addr = "加入多播组的主机地址信息";
 // setsockopt(send_sock, IPPROTO_IP, IP_ADD_MEMBERSHIP, (void*)&join, sizeof(join));
 
+#if defined(__linux__) || defined(__unix__)
+
 void multicast_server(int port) {
     sockaddr_in addr;
 
@@ -88,11 +90,154 @@ void multicast_client(int port) {
     close(sock);
 }
 
+//广播
+//多播即使在跨越不同网络的情况下，只要加入多播组就能接收数据，相反，广播只能向同一网络中的主机传输数据。
+//广播也是基于UDP完成的。
+//1、直接广播
+//2、本地广播
+//直接广播的IP地址除网络地址外，主机地址全部为1，比如向192.168.1中所有主机传输数据，可以向 192.168.1.255传输。
+//本地广播使用的IP地址限定为 255.255.255.255，比如192。168.1主机向255.255.255.255传输数据，数据将传递到192.168.1所有主机
+
+//默认生成的套接字会阻止广播
+//int bcast = 1;
+//setsockopt(sock, SOL_SOCKET, SO_BROADCAST, &bcast, sizeof(bcast));
+
+void broadcast_server(int port) {
+    sockaddr_in addr;
+    int sock = socket(AF_INET, SOCK_DGRAM, 0);
+    addr.sin_family = AF_INET;
+    addr.sin_port = htons(port);
+    addr.sin_addr.s_addr = inet_addr("255.255.255.255");
+
+    int bcast = 1;
+    setsockopt(sock, SOL_SOCKET, SO_BROADCAST, &bcast, sizeof(bcast));
+
+    char buf[256] = {0};
+
+    while (1) {
+        fgets(buf, sizeof(buf), stdin);
+
+        if (strncmp(buf, "q", 1) == 0 ||
+            strncmp(buf, "Q", 1) == 0) {
+            break;
+        }
+
+        sendto(sock, buf, strlen(buf), 0, (struct sockaddr *) &addr, sizeof(addr));
+    }
+    close(sock);
+}
+
+void broadcast_client(int port) {
+    sockaddr_in addr;
+    int sock = socket(AF_INET, SOCK_DGRAM, 0);
+    addr.sin_family = AF_INET;
+    addr.sin_port = htons(port);
+    addr.sin_addr.s_addr = htonl(INADDR_ANY);
+
+    bind(sock, (struct sockaddr *) &addr, sizeof(addr));
+
+    int len;
+    char buf[256] = {0};
+
+    while (1) {
+        len = recvfrom(sock, buf, sizeof(buf), 0, NULL, 0);
+        if (len < 0) {
+            break;
+        }
+        buf[len] = '\0';
+        printf("%s\n", buf);
+    }
+    close(sock);
+}
+
+#else
+
+void windows_multicast_server(int port) {
+    WSADATA wsaData;
+    if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) {
+        printf("WSAStartup error\n");
+        return;
+    }
+    SOCKADDR_IN addr;
+
+    int sock = socket(AF_INET, SOCK_DGRAM, 0);
+    addr.sin_family = AF_INET;
+    addr.sin_port = htons(port); //多播端口
+    addr.sin_addr.s_addr = inet_addr("224.1.1.1"); //多播组IP
+
+    int ttl = 64;
+    setsockopt(sock, IPPROTO_IP, IP_MULTICAST_TTL, (char *) &ttl, sizeof(ttl));
+
+    char buf[256] = {0};
+    while (1) {
+        fgets(buf, sizeof(buf), stdin);
+
+        if (strncmp(buf, "q", 1) == 0 ||
+            strncmp(buf, "Q", 1) == 0) {
+            break;
+        }
+
+        sendto(sock, buf, strlen(buf), 0, (struct sockaddr *) &addr, sizeof(addr));
+    }
+
+    closesocket(sock);
+    WSACleanup();
+}
+
+void windows_multicast_client(int port) {
+    WSADATA wsaData;
+    if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) {
+        printf("WSAStartup error\n");
+        return;
+    }
+    SOCKADDR_IN addr;
+    int sock = socket(AF_INET, SOCK_DGRAM, 0);
+    addr.sin_family = AF_INET;
+    addr.sin_port = htons(port);
+    addr.sin_addr.s_addr = htonl(INADDR_ANY);
+
+    struct ip_mreq req;
+    req.imr_multiaddr.s_addr = inet_addr("224.1.1.1");
+    req.imr_interface.s_addr = htonl(INADDR_ANY);
+
+    //这里需要bind，不然无法收到消息
+    bind(sock, (struct sockaddr *) &addr, sizeof(addr));
+
+    //加入多播组
+    setsockopt(sock, IPPROTO_IP, IP_ADD_MEMBERSHIP, (char *) &req, sizeof(req));
+
+    int len;
+    char buf[256] = {0};
+    while (1) {
+        len = recvfrom(sock, buf, sizeof(buf), 0, NULL, 0);
+        if (len < 0) {
+            break;
+        }
+        buf[len] = '\0';
+        printf("%s\n", buf);
+    }
+
+    closesocket(sock);
+    WSACleanup();
+}
+
+#endif
+
 int main(int argc, char *argv[]) {
+    //if (strcmp(argv[1], "server") == 0) {
+    //    multicast_server(atoi(argv[2]));
+    //} else if (strcmp(argv[1], "client") == 0) {
+    //    multicast_client(atoi(argv[2]));
+    //}
+    //if (strcmp(argv[1], "server") == 0) {
+    //    broadcast_server(atoi(argv[2]));
+    //} else if (strcmp(argv[1], "client") == 0) {
+    //    broadcast_client(atoi(argv[2]));
+    //}
     if (strcmp(argv[1], "server") == 0) {
-        multicast_server(atoi(argv[2]));
+        windows_multicast_server(atoi(argv[2]));
     } else if (strcmp(argv[1], "client") == 0) {
-        multicast_client(atoi(argv[2]));
+        windows_multicast_client(atoi(argv[2]));
     }
     return 0;
 }
